f156ae9d62e96e153335f55c46d1e4365febd3a8
Caleb Lemoine <circa10a@users.noreply.github.com>
2017-02-25 13:22:13 -0600

    Update notifications.js

--- monitor/js/notifications.js
+++ monitor/js/notifications.js
@@ -1,27 +1,3 @@
- var isMobile = {
-    Android: function() {
-        return navigator.userAgent.match(/Android/i);
-    },
-    BlackBerry: function() {
-        return navigator.userAgent.match(/BlackBerry/i);
-    },
-    iOS: function() {
-        return navigator.userAgent.match(/iPhone|iPod/i);
-    },
-    Opera: function() {
-        return navigator.userAgent.match(/Opera Mini/i);
-    },
-    Windows: function() {
-        return navigator.userAgent.match(/IEMobile/i);
-    },
-    any: function() {
-        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-    }
-};
-
-if(isMobile.any()) {
-
-} else {
 var value = $('#circle').circleProgress('value');
 if (value >= 1.0){
   //$('#circle').circleProgress({ fill: "green" });
@@ -52,6 +28,5 @@ if (value >= 1.0){
                      easing: 'swing',
                  }
 
-  });
- }
+ });
 }
