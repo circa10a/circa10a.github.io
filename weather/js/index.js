35906b6653001b85453fa8faa1995075a49b528f
circa10a <caleblemoine@gmail.comS>
2017-05-29 00:50:37 -0500

    add weather project

---
+++ weather/js/index.js
@@ -0,0 +1,52 @@
+//Docs at http://simpleweatherjs.com
+// Docs at http://simpleweatherjs.com
+
+  navigator.geolocation.getCurrentPosition(function(position) {
+    $.LoadingOverlay("show");
+    loadWeather(position.coords.latitude+','+position.coords.longitude); //load weather using your lat/lng coordinates
+  });
+
+$(document).ready(function() {
+  loadWeather(); //@params location, woeid
+});
+
+function loadWeather(location, woeid) {
+  $.simpleWeather({
+    location: location,
+    woeid: woeid,
+    unit: 'f',
+    success: function(weather) {
+      html = '<h2>'+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+      html += '<li class="currently">'+weather.currently+'</li>';
+      html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
+
+      $("#weather").html(html);
+      $("#search").attr("placeholder", weather.city + ", " +weather.region);
+
+      $.LoadingOverlay("hide");
+    },
+    error: function(error) {
+      $("#weather").html('<p>'+error+'</p>');
+    }
+  });
+}
+
+$("#button").click(function() {
+  $.simpleWeather({
+    location: $("#search").val(),
+    woeid: '',
+    unit: 'f',
+    success: function(weather) {
+      html = '<h2>'+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+      html += '<li class="currently">'+weather.currently+'</li>';
+      html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
+
+      $("#weather").html(html);
+    },
+    error: function(error) {
+      $("#weather").html('<p>'+error+'</p>');
+    }
+  });
+});
